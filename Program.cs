cba09a5e125476d93762037d28a91e0098fc9281
svdycl2858@gmail.com <svdycl2858@gmail.com>
2021-01-19 19:52:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Adi = "elma";
+            urun1.Fiyati = 100;
+            urun1.Aciklama = "amasya elması";
+            urun1.stokAdedi = 5;
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "diyarbakır karpuzu";
+            urun2.stokAdedi = 6;
+
+
+            Urun[] urunler = new Urun[] {urun1,urun2 };
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.stokAdedi);
+                Console.WriteLine("------------");
+
+            }
+            Console.WriteLine("-----------metodlar-----------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "yeşil", 12,10);
+            sepetManager.Ekle2("elma", "yeşil", 18,9);
+            sepetManager.Ekle2("karpuz", "diyarbakır", 15,6);
+        }
+    }
+    }
---
+++ SepetManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metodlar
+{
+    class SepetManager
+    {
+        //naming convention isimlendirme kuralı
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("sepete eklendi" + urun.Adi);
+        
+        }
+        public void Ekle2(string Adi, string Aciklama, double Fiyati, int stokAdedi)
+        {
+            Console.WriteLine("tebrikler sepete eklendi");
+        
+        }
+
+    }
+}
